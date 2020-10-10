541f792ea654cce0bd5277cd2448b689f3b15eba
Giscam2501 <60549154+Giscam2501@users.noreply.github.com>
2020-10-10 17:52:14 -0500

    Create HELLO.java

---
+++ HELLO.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Hello {
+    public void sayHello() {
+        System.out.println("Hello, my name is Gisela");
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+        Hello hello = new Hello();
+        hello.sayHello();
+    }
+}
