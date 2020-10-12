6735af4048ea6e092c945d1d0890e91cb6494973
Carlos Alberto León Chinchay <cleon@dosx0099.home>
2020-10-12 17:27:03 +0200

    add greetings from carlos

--- HELLO.java
+++ HELLO.java
@@ -3,7 +3,8 @@ package com.company;
 public class Hello {
     public void sayHello() {
         System.out.println("Hello, my name is Gisela");
-    }
+	System.out.println("Hello Gisela, this is Carlos")    
+	}
 
     public static void main(String[] args) {
         // write your code here
